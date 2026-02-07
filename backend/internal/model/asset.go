549238d660a438910757c624a49952bc385bf3bf
Ahmed Selim YILDIRIM <mac@Ahmed-Selim-YILDIRIM.local>
2026-02-07 12:14:56 +0300

    Açıklama

--- backend/internal/model/asset.go
+++ backend/internal/model/asset.go
@@ -6,13 +6,14 @@ import (
 	"github.com/uptrace/bun"
 )
 
+// internal/model/asset.go
 type Asset struct {
 	bun.BaseModel `bun:"table:assets,alias:a"`
-	ID            int64     `bun:",pk,autoincrement" json:"id"`
-	UserID        int64     `bun:"user_id,notnull,unique:user_asset_ayar" json:"user_id"`
-	Type          string    `bun:"type,notnull,unique:user_asset_ayar" json:"type"`
-	Amount        float64   `bun:"amount,notnull,default:0" json:"amount"`
-	Ayar          int       `bun:"ayar,default:24,unique:user_asset_ayar" json:"ayar"`
-	TotalCost     float64   `bun:"total_cost,default:0" json:"total_cost"`
-	UpdatedAt     time.Time `bun:",nullzero,notnull,default:current_timestamp" json:"updated_at"`
+	ID            int64     `bun:",pk,autoincrement"`
+	UserID        int64     `bun:"user_id,notnull,unique:user_asset_ayar"`
+	Type          string    `bun:"type,notnull,unique:user_asset_ayar"`
+	Ayar          int       `bun:"ayar,default:24,unique:user_asset_ayar"`
+	Amount        float64   `bun:"amount,notnull,default:0"`
+	TotalCost     float64   `bun:"total_cost,default:0"`
+	UpdatedAt     time.Time `bun:",nullzero,notnull,default:current_timestamp"`
 }
