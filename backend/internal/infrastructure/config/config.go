862e9202dead8b19e4d16b2d8be5754d296fd210
Ahmed Selim YILDIRIM <mac@Ahmed-Selim-YILDIRIM.local>
2026-02-08 13:30:53 +0300

    Açıklama

--- backend/internal/infrastructure/config/config.go
+++ backend/internal/infrastructure/config/config.go
@@ -7,35 +7,33 @@ import (
 )
 
 type Config struct {
-	DBHost            string
-	DBUser            string
-	DBPassword        string
-	DBName            string
-	DBPort            string
-	DBURL             string
-	JWTSecret         string
-	ExchangeAPIKey    string
-	CoinGeckoKey      string
-	MetalsAPIKey      string
-	CurrencyBeaconKey string
-	RedisURL          string
+	DBHost         string
+	DBUser         string
+	DBPassword     string
+	DBName         string
+	DBPort         string
+	DBURL          string
+	JWTSecret      string
+	ExchangeAPIKey string
+	CoinGeckoKey   string
+	MetalsAPIKey   string
+	RedisURL       string
 }
 
 func LoadConfig() *Config {
 	_ = godotenv.Load()
 
 	return &Config{
-		DBHost:            os.Getenv("DB_HOST"),
-		DBUser:            os.Getenv("DB_USER"),
-		DBPassword:        os.Getenv("DB_PASSWORD"),
-		DBName:            os.Getenv("DB_NAME"),
-		DBPort:            os.Getenv("DB_PORT"),
-		DBURL:             os.Getenv("DB_URL"),
-		JWTSecret:         os.Getenv("JWT_SECRET"),
-		ExchangeAPIKey:    os.Getenv("EXCHANGE_RATE_API_KEY"),
-		CoinGeckoKey:      os.Getenv("COINGECKO_API_KEY"),
-		MetalsAPIKey:      os.Getenv("METALS_API_KEY"),
-		CurrencyBeaconKey: os.Getenv("CURRENCY_BEACON_KEY"),
-		RedisURL:          os.Getenv("REDIS_URL"),
+		DBHost:         os.Getenv("DB_HOST"),
+		DBUser:         os.Getenv("DB_USER"),
+		DBPassword:     os.Getenv("DB_PASSWORD"),
+		DBName:         os.Getenv("DB_NAME"),
+		DBPort:         os.Getenv("DB_PORT"),
+		DBURL:          os.Getenv("DB_URL"),
+		JWTSecret:      os.Getenv("JWT_SECRET"),
+		ExchangeAPIKey: os.Getenv("EXCHANGE_RATE_API_KEY"),
+		CoinGeckoKey:   os.Getenv("COINGECKO_API_KEY"),
+		MetalsAPIKey:   os.Getenv("METALS_API_KEY"),
+		RedisURL:       os.Getenv("REDIS_URL"),
 	}
 }
