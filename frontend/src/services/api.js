895707dfde51e7c0a00f0585c20104bab2ca4e7d
Ahmed Selim YILDIRIM <mac@Ahmed-Selim-YILDIRIM.local>
2026-02-10 10:33:16 +0300

    Açıklama

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,14 +2,22 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://fin-track-pro.onrender.com/api/v1',
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-});
+);
 
 export default api;
