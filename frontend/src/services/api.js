174be977bfdac7f75720363410050f94d975ab84
Ahmed Selim YILDIRIM <ahmedselimyildirim@example.com>
2026-02-13 17:07:43 +0300

    Bun düzeltmeleri 11

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://fin-track-pro-1.onrender.com/api/v1',
+    baseURL: 'https://fin-track-pro.onrender.com/api/v1',
     headers: {
         'Content-Type': 'application/json'
     }
